e1e1f0b6d776e2c80eabc4e80ab5ca25fcbc8e77
matt box <matt.box88@gmail.com>
2025-08-02 16:35:20 -0400

    add first four exercises from variables and operators course

---
+++ variablesAndOperators/initialization.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+  int numOfBooks = 5;
+  char favLetter = 'B';
+  char favDigit = '8';
+  double costOfCandyBar = 1.50;
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your Favorite Letter is: %c\n", favLetter);
+  printf("Your Favorite Digit is: %c\n", favDigit);
+  printf("You expect to pay $%.2f for a candy bar.\n", costOfCandyBar);
+}
