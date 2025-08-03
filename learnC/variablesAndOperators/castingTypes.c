d6f3d4bb5d5399facd044e9b0f9323e201614bbe
matt box <matt.box88@gmail.com>
2025-08-02 20:34:47 -0400

    add exercises from module 1 part 2

---
+++ learnC/variablesAndOperators/castingTypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+  double testScore = 95.7;
+  int displayScore = (int)testScore;
+
+  // No need to change below here
+  printf("Great work, you got a %d%% on your test\n", displayScore);
+}
