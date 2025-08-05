99c9ff9512b48d4bf240f767397e2833ab6f94ff
matt box <matt.box88@gmail.com>
2025-08-04 20:21:45 -0400

    add header to output table

--- KnR/fahrenheitCelsius2.c
+++ KnR/fahrenheitCelsius2.c
@@ -8,10 +8,12 @@ int main() {
   upper = 300;
   step = 20;
 
+  printf("Fahr. Celsius\n=============\n");
+
   fahr = lower;
   while (fahr <= upper) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
+    printf("%4.0f %6.1f\n", fahr, celsius);
     fahr += step;
   }
 }
