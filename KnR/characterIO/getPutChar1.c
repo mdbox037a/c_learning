ba2d876dc834b77f1dfb9c4f7f1044d74f56c631
matt box <matt.box88@gmail.com>
2025-08-08 21:39:22 -0400

    add comments to clarify int choice and EOF

--- KnR/characterIO/getPutChar1.c
+++ KnR/characterIO/getPutChar1.c
@@ -2,10 +2,10 @@
 
 /*copy input to output,version 1*/
 int main() {
-  int c;
+  int c; // int to hold large enough values
 
   c = getchar();
-  while (c != EOF) {
+  while (c != EOF) { // EOF is an integer defined in <stdio.h>
     putchar(c);
     c = getchar();
   }
