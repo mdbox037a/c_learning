6d4d8f973f3e7cfe3ff781c265a95b1ae1bcebce
matt box <matt.box88@gmail.com>
2025-08-09 08:00:39 -0400

    revise program to print more info about EOF value and truth status of while loop

--- KnR/characterIO/eofCheck.c
+++ KnR/characterIO/eofCheck.c
@@ -4,8 +4,11 @@ int c;
 int check;
 
 int main() {
+  printf("hint: to kill this, press Ctrl-D\nEnter any text below:\n");
+  printf("EOF value is: %i\n", EOF);
+
   while ((c = getchar()) != EOF) {
+    printf("\n%i: ", check = (c != EOF));
     putchar(c);
-    printf("%i", check = (c != EOF));
   }
 }
