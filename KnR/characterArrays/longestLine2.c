4dc4bc6dd3cdcdadeeb99d7d89f19a65224fbbde
matt box <mdbox037a@protonmail.com>
2025-08-29 23:12:42 -0400

    attempt solution to exercise 1-16; not final yet -- need to revise

--- KnR/characterArrays/longestLine2.c
+++ KnR/characterArrays/longestLine2.c
@@ -1,8 +1,10 @@
+// TODO: fix logic here - something is very wrong
+#include <stdint.h>
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
 
 int get_line(char line[], int maxline);
-void copy(char to[], char from[]);
+void copy(char to[], char from[], int maxline);
 
 /* print the longest input line;
  * exercise 1-16: revise the main routine so it will print the length of
@@ -17,32 +19,39 @@ int main() {
 	while ((len = get_line(line, MAXLINE)) > 0)
 		if (len > max) {
 			max = len;
-			copy(longest, line);
+			copy(longest, line, MAXLINE);
 		}
-	if (max > 0)
-		printf("%s", longest);
+	if (max > 0) {
+		printf("%s\n", longest);
+		printf("That string was %d characters long\n", max);
+	}
 	return 0;
 }
 
 /* getline: read a line into s, return length */
 int get_line(char s[], int lim) {
-	int c, i;
+	int c;
+	int32_t i;
 
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
 		s[i] = c;
+		++i;
+	}
 	if (c == '\n') { // one more round at newline
 		s[i] = c;
 		++i;
 	}
 	s[i] = '\0'; // place null character at end of array
+	while ((c = getchar()) != EOF && c != '\n')
+		++i;
 	return i;
 }
 
 /* copy: copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[]) {
+void copy(char to[], char from[], int lim) {
 	int i;
 
 	i = 0;
-	while ((to[i] = from[i]) != '\0')
+	while ((to[i] = from[i]) != '\0' && i < lim)
 		++i;
 }
