e30d8a2ef7de6db30833ac829d3cf0b181f2127c
matt box <mdbox037a@protonmail.com>
2025-08-30 22:06:32 -0400

    adapt longestLine program to exercise 1-16: track the length of arbitrarily long strings and print as much of the string as possible

---
+++ KnR/characterArrays/longestLine3.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum input line length */
+
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print the longest input line */
+int main() {
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	while ((len = get_line(line, MAXLINE)) > 0)
+		if (len > max) {
+			max = len;
+			copy(longest, line);
+		}
+	if (max > 0) {
+		printf("\n=====RESULTS=====\n");
+		printf("the longest string had %d characters:\n", max);
+		printf("%s", longest);
+	}
+
+	return 0;
+}
+
+/* getline: read a line into s, return length */
+int get_line(char s[], int lim) {
+	int c, i;
+
+	// remove stopper at MAXIMUM and then add if/else chain to handle cases
+	for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
+		if (i < lim - 1)
+			s[i] = c;
+		else if (i == lim)
+			s[i] = '\0';
+		else
+			;
+	}
+	if (c == '\n') { // one more round at newline
+		if (i < lim - 1) {
+			s[i] = c;
+			++i;
+		} else if (i == lim) {
+			s[i - 1] = '\n';
+			s[i] = '\0';
+			++i;
+		} else {
+			++i;
+		}
+	}
+
+	return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[]) {
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
