a86725bc3a5fc5292261c87e7dd28ae7bd002ccd
matt box <mdbox037a@protonmail.com>
2025-08-31 21:44:11 -0400

    feat: add hold of 80-char arrays until EOF

--- KnR/characterArrays/print80.c
+++ KnR/characterArrays/print80.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
-#define MAXIMUM 1000
+#define MAX_ARRAY_SIZE 1000
+#define MAX_NUM_ARRAYS 50
 #define BAR 80
 
 // TODO: need to alter logic to hold printing of 80+ lines until EOF
+
+/* planning logic changes
+ * if string_length >= 80, create a new array and copy string there
+ * keep track of string names in another array
+ * then iterate over string array and print at end
+ * */
+
 int line_reader(char line[], int max_length);
+void copy(char to[], char from[]);
 
 int main() {
-	int string_length, i;
-	char line[MAXIMUM];
+	int string_length, i, j, k, current_array;
+	char line[MAX_ARRAY_SIZE];
+	// implement a 'saver' array of 80+ char arrays
+	char size80_arrays[MAX_NUM_ARRAYS][MAX_ARRAY_SIZE];
 
-	while ((string_length = line_reader(line, MAXIMUM)) > 0) {
-		if (string_length >= BAR)
-			printf("%s", line);
+	current_array = 0;
+	// set all values in multD array to 0; not sure this is necessry tho...
+	for (j = 0; j < MAX_NUM_ARRAYS; ++j)
+		for (k = 0; k < MAX_ARRAY_SIZE; ++k)
+			size80_arrays[j][k] = 0;
+
+	while ((string_length = line_reader(line, MAX_ARRAY_SIZE)) > 0) {
+		if (string_length >= BAR) {
+			copy(size80_arrays[current_array], line);
+			++current_array; // get set for next array in multD
+			                 // array
+		}
+	}
+
+	// printout all 80+ char arrays
+	for (i = 0; i <= current_array; ++i) {
+		printf("%s\n", size80_arrays[i]);
+		// TODO: prettyfy this output?
 	}
 
 	return 0;
@@ -29,4 +55,12 @@ int line_reader(char line[], int lim) {
 	line[i] = '\0';
 
 	return i;
+}
+
+void copy(char to[], char from[]) {
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
 }
