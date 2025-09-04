8ff01798c9f4d5ab322669f2e435698dd8ac7808
matt box <mdbox037a@protonmail.com>
2025-09-03 21:51:16 -0400

    complete first draft of detab

--- KnR/cCore/detab.c
+++ KnR/cCore/detab.c
@@ -9,3 +9,38 @@
  * */
 
 #include <stdio.h>
+#define MAX_LEN 1000
+#define TAB_SIZE 4
+
+int get_line_detab(char line[]);
+
+int main() {
+	char line[MAX_LEN];
+
+	while ((get_line_detab(line)) > 0)
+		printf("%s\n", line);
+
+	return 0;
+}
+
+int get_line_detab(char line[]) {
+	int c, i, j;
+
+	for (i = 0; i < MAX_LEN - 1 && (c = getchar()) != EOF && c != '\n';
+	     ++i) {
+		printf("%d", (TAB_SIZE - (i % TAB_SIZE)));
+		if (c == '\t') {
+			for (j = 0; j < (TAB_SIZE - (i % TAB_SIZE)); ++j) {
+				line[i] = ' ';
+				++i;
+			}
+		} else
+			line[i] = c;
+	}
+	if (c == '\n') {
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
+}
