c14df122e3a486595c8fa8f1395749d4a1b1bfba
matt box <mdbox037a@protonmail.com>
2025-09-06 21:26:44 -0400

    create textWrap.c stub

---
+++ KnR/cCore/textWrap.c
@@ -0,0 +1,4 @@
+/* Exercise 1-22: Write a program to fold long lines into two or more shorter
+ * lines after the last non-blank character that occurs before the nth
+ * column of input.  Make sure to do something intelligent with very long lines
+ * and if there are no blanks or tabs before the specified column */
