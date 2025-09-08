0a38a87d3d143171032b3f306a67747f6e76491c
matt box <mdbox037a@protonmail.com>
2025-09-07 23:53:28 -0400

    complete in_comment == 0 logic

--- KnR/cCore/removeComments.c
+++ KnR/cCore/removeComments.c
@@ -6,7 +6,24 @@
 
 int main() {
 	int c;
-	while ((c = getchar()) != EOF) {
-		printf("%c", c);
+	int i = 0;
+	int in_comment = 0;
+
+	char buffer[3];
+	buffer[2] = '\0';
+
+	while ((c = getchar()) != EOF && in_comment == 0) {
+		if (i == 0) {
+			buffer[0] = c;
+			++i;
+		} else if (i == 1) {
+			buffer[1] = c;
+			if (buffer[0] == '/' && buffer[1] == '*')
+				in_comment = 1;
+			else {
+				printf("%c", buffer[0]);
+				buffer[0] = buffer[1];
+			}
+		}
 	}
 }
