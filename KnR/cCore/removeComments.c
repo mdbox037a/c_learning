4ba8d72bffe134c12c9c93048a7e7b5a59d351cf
matt box <mdbox037a@protonmail.com>
2025-09-08 23:21:41 -0400

    implement functions for print/hold buffer; TODO: repair logic to successfully escape in_comment = 1

--- KnR/cCore/removeComments.c
+++ KnR/cCore/removeComments.c
@@ -4,8 +4,8 @@
 
 #include <stdio.h>
 
-int print_buffer(char buffer[], int in_comment);
-int hold_buffer(char buffer[], int in_comment);
+int print_buffer(char buffer[], int c, int in_comment);
+int hold_buffer(char buffer[], int c, int in_comment);
 
 int main() {
 	int c;
@@ -15,27 +15,37 @@ int main() {
 	char buffer[3];
 	buffer[2] = '\0';
 
-	while ((c = getchar()) != EOF && in_comment == 0) {
-		if (i == 0) {
-			buffer[0] = c;
-			++i;
-		} else if (i == 1) {
-			buffer[1] = c;
-			if (buffer[0] == '/' && buffer[1] == '*')
-				in_comment = 1;
-			else {
-				printf("%c", buffer[0]);
-				buffer[0] = buffer[1];
-			}
-		}
+	while ((c = getchar()) != EOF) {
+		if (in_comment == 0)
+			print_buffer(buffer, c, in_comment);
+		else if (in_comment == 1)
+			hold_buffer(buffer, c, in_comment);
 	}
+}
 
-	while ((c = getchar()) != EOF && in_comment == 1) {
+int print_buffer(char buffer[], int c, int in_comment) {
+	int i = 0;
+
+	if (i == 0) {
+		buffer[0] = c;
+		++i;
+	} else if (i == 1) {
 		buffer[1] = c;
-		if (buffer[0] == '*' && buffer[1] == '/')
-			in_comment = 0;
+		if (buffer[0] == '/' && buffer[1] == '*')
+			in_comment = 1;
 		else {
+			printf("%c", buffer[0]);
 			buffer[0] = buffer[1];
 		}
 	}
+	return in_comment;
+}
+
+int hold_buffer(char buffer[], int c, int in_comment) {
+	buffer[1] = c;
+	if (buffer[0] == '*' && buffer[1] == '/')
+		in_comment = 0;
+	else
+		buffer[0] = buffer[1];
+	return in_comment;
 }
