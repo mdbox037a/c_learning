25b44d3bca58a9c95cee7f91e4a203191e8f4e6c
matt box <mdbox037a@protonmail.com>
2025-09-11 19:16:38 -0400

    complete logic to ensure leading / is removed

--- KnR/cCore/removeComments.c
+++ KnR/cCore/removeComments.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 int print_buffer(char buffer[], int c, int in_comment);
+int maybe_buffer(char buffer[], int c, int in_comment);
 int hold_buffer(char buffer[], int c, int in_comment);
 
 int main() {
@@ -22,6 +23,8 @@ int main() {
 		// had to assign function return value to in_comment
 		// in order to have function local var by same name
 		// get back "out" of the function. lol
+		else if (in_comment == -1)
+			in_comment = maybe_buffer(buffer, c, in_comment);
 		else
 			in_comment = hold_buffer(buffer, c, in_comment);
 	}
@@ -31,11 +34,25 @@ int print_buffer(char buffer[], int c, int in_comment) {
 	buffer[1] = c;
 	if (buffer[0] == '/' && buffer[1] == '*')
 		in_comment = 1;
+	else if (buffer[1] == '/')
+		in_comment = -1;
 	else {
 		// TODO: now the comment is almost all removed, except for the
 		// first '/' --> need to figure this out
 		printf("%c", buffer[1]);
 		buffer[0] = buffer[1];
+	}
+	return in_comment;
+}
+
+int maybe_buffer(char buffer[], int c, int in_comment) {
+	buffer[0] = buffer[1];
+	buffer[1] = c;
+	if (buffer[0] == '/' && buffer[1] == '*')
+		in_comment = 1;
+	else {
+		in_comment = 0;
+		printf("%c%c", buffer[0], buffer[1]);
 	}
 	return in_comment;
 }
