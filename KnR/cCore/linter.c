2214ed0efe2db578756c3f23bff218f86bfaa9ac
matt box <mdbox037a@protonmail.com>
2025-09-15 22:27:12 -0400

    complete logic for tracking not to happen during comments

--- KnR/cCore/linter.c
+++ KnR/cCore/linter.c
@@ -3,29 +3,69 @@
  * about quotes, both single and double, escape sequences, and comments */
 
 #include <stdio.h>
+#define BUFFERLEN 3
 int characters[256];
+char buffer[BUFFERLEN];
 
-void track_characters(int c);
+int track_characters(int c, int in_comment);
 void check_pairs();
 
 int main() {
 	int c, i, status;
+	int in_comment = 0;
 	extern int characters[];
+	extern char buffer[];
+
+	for (i = 0; i < BUFFERLEN; ++i)
+		buffer[i] = '\0';
 
 	while ((c = getchar()) != EOF) {
-		track_characters(c);
+		in_comment = track_characters(c, in_comment);
 	}
 	check_pairs();
 
-	// for (i = 0; i < 256; ++i) {
-	//	printf("%c:%d\n", i, characters[i]);
-	// }
 	return 0;
 }
 
-void track_characters(int c) {
+int track_characters(int c, int in_comment) {
 	extern int characters[];
-	characters[c] += 1;
+	extern char buffer[];
+
+	if (in_comment == 0) {
+		buffer[1] = c;
+		if (buffer[0] == '/' && buffer[1] == '*')
+			in_comment = 1;
+		else if (buffer[1] == '/')
+			in_comment = -1;
+		else {
+			buffer[0] = buffer[1];
+			characters[buffer[1]] += 1;
+		}
+		return in_comment;
+	}
+
+	if (in_comment == -1) {
+		buffer[0] = buffer[1];
+		buffer[1] = c;
+		if (buffer[0] == '/' && buffer[1] == '*')
+			in_comment = 1;
+		else {
+			in_comment = 0;
+			characters[buffer[0]] += 1;
+			characters[buffer[1]] += 1;
+		}
+		return in_comment;
+	}
+
+	if (in_comment == 1) {
+		buffer[1] = c;
+		if (buffer[0] == '*' && buffer[1] == '/')
+			in_comment = 0;
+		else
+			buffer[0] = buffer[1];
+		return in_comment;
+	}
+	return in_comment;
 }
 
 void check_pairs() {
@@ -42,4 +82,4 @@ void check_pairs() {
 		printf("unclosed single quotations marks\n");
 }
 
-// TODO: begin plan for escape sequences and comments linting
+// TODO: implement escape character logic
