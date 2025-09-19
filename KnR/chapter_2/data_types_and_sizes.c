3ee1c7fdbd2212d7b809ad6c92766e7bbcfd0b94
matt box <mdbox037a@protonmail.com>
2025-09-18 22:59:04 -0400

    refine output formatting

--- KnR/chapter_2/data_types_and_sizes.c
+++ KnR/chapter_2/data_types_and_sizes.c
@@ -12,16 +12,19 @@ int main() {
 	int integer;
 	long longy;
 
+	printf("##### from macros #####\n");
+	printf("----- signed int/char types -----\n");
 	printf("char: %d to %d bits\n", CHAR_MIN, CHAR_MAX);
 	printf("signed char: %d to %d bits\n", SCHAR_MIN, SCHAR_MAX);
 	printf("short: %d to %d bits\n", SHRT_MIN, SHRT_MAX);
 	printf("int: %d to %d bits\n", INT_MIN, INT_MAX);
 	printf("long: %ld to %ld bits\n", LONG_MIN, LONG_MAX);
-	printf("-----unsigned types-----\n");
+	printf("----- unsigned int/char types -----\n");
 	printf("uchar: %d\n", UCHAR_MAX);
 	printf("ushort: %d\n", USHRT_MAX);
 	printf("uint: %d\n", UINT_MAX);
 	printf("ulong: %ld\n", ULONG_MAX);
+	printf("\n");
 
 	return 0;
 }
