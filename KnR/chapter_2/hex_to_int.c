8f047cbc8b82769ae7b7eddfe0b33c6ac9350c9c
matt box <mdbox037a@protonmail.com>
2025-09-24 21:29:58 -0400

    complete htoi logic to convert hex to decimal

--- KnR/chapter_2/hex_to_int.c
+++ KnR/chapter_2/hex_to_int.c
@@ -2,6 +2,7 @@
  * digits (including and optional 0x or 0X) into its equivalent integer value.
  * The allowable digits are 0 though 9, a through f, and A through F */
 
+#include <math.h>
 #include <stdio.h>
 
 #define HEX_LEN 11
@@ -13,23 +14,36 @@ int main() {
 	int result;
 
 	result = htoi(hex_array);
-	printf("original hex: %c\n", hex_array);
+	printf("original hex: %s\n", hex_array);
 	printf("convert to int: %d\n", result);
 	return 0;
 }
 
 int htoi(char hex_array[]) {
-	int i, c, integer;
+	int i, c, counter, base, dec_value, total;
+	counter = total = 0;
+	base = 16;
 
-	for (i = 0; i < HEX_LEN; ++i) {
+	for (i = HEX_LEN - 2; i >= 0; --i) {
 		c = hex_array[i];
+		// printf("c: %c\n", c);
 		if (c == 0 && i == 0)
 			;
 		else if (c == 'x' || c == 'X')
 			;
 		else {
-			// insert conversoion logic here
+			if ('0' <= c && c <= '9')
+				dec_value = c - '0';
+			else if ('a' <= c && c <= 'f')
+				dec_value = c - 'a' + 10;
+			else if ('A' <= c && c <= 'F')
+				dec_value = c - 'A' + 10;
+			else
+				return -1;
+			total += dec_value * pow((double)base, (double)counter);
+			// printf("total: %d\n", total);
+			++counter;
 		}
 	}
-	return integer;
+	return total;
 }
