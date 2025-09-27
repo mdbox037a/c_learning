b8bfec0beed819abd6bf5a922a66a1586b35090c
matt box <mdbox037a@protonmail.com>
2025-09-26 22:04:13 -0400

    further flesh out planning for anti-string program

--- KnR/chapter_2/anti_string.c
+++ KnR/chapter_2/anti_string.c
@@ -1,6 +1,20 @@
 /* Exercise 2-4: Write and alternative version of squeeze(s1,s2) that deletes
  * each character in s1 that matches any character in the string s2 */
 
+#include <stdio.h>
+
+void anti_string(char s1[], char s2[]);
+
+int main() {
+	;
+	// input and call function logic here
+}
+
+void anti_string(char s1[], char s2[]) {
+	;
+	// removal logic here
+}
+
 // original squeeze from KnR:
 
 /* squeeze: delete all c from s */
