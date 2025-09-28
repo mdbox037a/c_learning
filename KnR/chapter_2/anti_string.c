1e4bd9e1c0c41c35e8759d81f07bd03cfc469f0d
matt box <mdbox037a@protonmail.com>
2025-09-27 21:48:24 -0400

    complete scenario unsatisfactorily --> replace matching chars with BEL char which is not rendered when string is output

--- KnR/chapter_2/anti_string.c
+++ KnR/chapter_2/anti_string.c
@@ -33,8 +33,7 @@ void anti_string(char s1[], char s2[]) {
 	for (i = 0; s1[i] != '\0'; ++i) {
 		for (j = 0; s2[j] != '\0'; ++j) {
 			if (s1[i] == s2[j]) {
-				s1[i] = s1[i + 1];
-				// TODO: add logic to handle "s1: aa" case
+				s1[i] = '\a';
 			}
 		}
 	}
