f3696683f7a09689ea2629389aa74a83e0bebf81
matt box <mdbox037a@protonmail.com>
2025-09-28 21:59:23 -0400

    create any.c skeleton

---
+++ KnR/chapter_2/any.c
@@ -0,0 +1,16 @@
+/* Exercise 2-5: Write the function sny(s1, s2), which returns the first
+ * location in a string s1 where any character from the string s2 occurs,
+ * or -1 if s1 contains no characters from s2. */
+
+#include <stdio.h>
+
+void any(char s1[], char s2[]);
+
+int main() {
+	// function to accept s1 and s2 input from user
+	// call any() to filter and return
+}
+
+void any(char s1[], s2[]) {
+	// accept s1 and s2 from main() and filter by rules
+}
