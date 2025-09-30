c310ddea4c9ca9728a4be70e94ab6ec08b640073
matt box <mdbox037a@protonmail.com>
2025-09-29 21:12:13 -0400

    complete user input section and validate logic

--- KnR/chapter_2/any.c
+++ KnR/chapter_2/any.c
@@ -10,6 +10,22 @@ int any(char s1[], char s2[]);
 int main() {
 	// function to accept s1 and s2 input from user
 	// call any() to filter and return
+	int i, c, match;
+	char s1[MAX_STR_LEN], s2[MAX_STR_LEN];
+
+	printf("Enter a string: ");
+	for (i = 0; (c = getchar()) != '\n' && i < MAX_STR_LEN - 1; ++i) {
+		s1[i] = c;
+	}
+	printf("Enter a filter string: ");
+	for (i = 0; (c = getchar()) != '\n' && i < MAX_STR_LEN - 1; ++i) {
+		s2[i] = c;
+	}
+
+	match = any(s1, s2);
+	printf("Location in s1 of first match: %d\n", match);
+
+	return 0;
 }
 
 int any(char s1[], char s2[]) {
