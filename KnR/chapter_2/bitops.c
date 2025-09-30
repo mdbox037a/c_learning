0ca5cc730dfb49ba168949265f906e3bce66be71
matt box <mdbox037a@protonmail.com>
2025-09-29 22:46:18 -0400

    complete logic for exercise 2-6

--- KnR/chapter_2/bitops.c
+++ KnR/chapter_2/bitops.c
@@ -2,3 +2,12 @@
 unsigned getbits(unsigned x, int p, int n) {
 	return (x >> (p + 1 - n)) & ~(~0 << n);
 }
+
+/* Exercise 2-6: Write a function setbits(x,p,n,y) that returns x with the n
+ * bits that begin at position p set to the rightmost n bits of y, leaving
+ * the other bits unchanged */
+
+unsigned setbits(unsigned x, int p, int n, unsigned y) {
+	return (x & ~((~(~0 << n)) << (p + 1 - n))) |
+	       ((y & (~(~0 << n))) << (p + 1 - n));
+}
