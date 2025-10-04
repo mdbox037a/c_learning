f39e89e5c970d9c7f17a7dd96e58bb6f211be18d
matt box <mdbox037a@protonmail.com>
2025-10-03 22:32:18 -0400

    add skeleton for better_lower() - an improvement to lower() that uses a conditional expression

---
+++ KnR/chapter_2/better_lower.c
@@ -0,0 +1,15 @@
+/* Exercise 2-10: Rewrite teh function lower, which converts upper case letters
+ * to lower case, with a conditional expression instead if-else */
+
+// original lower
+
+int lower(int c) {
+	if (c >= 'A' && c <= 'Z')
+		return c + 'a' - 'A';
+	else
+		return c;
+}
+
+// (hopefully) improved
+
+int better_lower(int c) { return c; }
