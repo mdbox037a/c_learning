1cfa4e62f6b66de80fd595ff7e2f07247d2bbc98
matt box <mdbox037a@protonmail.com>
2025-10-04 18:12:23 -0400

    create skeleton for smaller loop version of binsearch()

--- KnR/chapter_3/binsearch_elif.c
+++ KnR/chapter_3/binsearch_elif.c
@@ -15,3 +15,16 @@ int binsearch(int x, int v[], int n) {
 	}
 	return -1; /* no match */
 }
+
+/* Exercise 3-1: Our binary search makes two tests inside the loop, when one
+ * would suffice (at the price of more tests outside). Write a version with
+ * only one test inside the loop and measure the difference in run-time */
+
+int binsearch_v2(int x, int v[], int n) {
+	int low, mid, high;
+
+	low = 0;
+	high = n - 1;
+	// loop here
+	return -1;
+}
