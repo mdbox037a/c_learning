51c121f3a1a5346df5ad73348e09e029f91cbb04
matt box <mdbox037a@protonmail.com>
2025-10-06 21:05:07 -0400

    resolve script issues with break statements and nesting switch chain within while loop

--- KnR/chapter_3/bad_8_ball.c
+++ KnR/chapter_3/bad_8_ball.c
@@ -4,28 +4,36 @@ int main() {
 	char c;
 
 	printf("Think of your question, then enter a number from 1 to 8: ");
-	while ((c = getchar()) != '\n')
-		;
-
-	switch (c) {
-	case '1':
-		printf("Sometimes good, sometimes shit\n");
-	case '2':
-		printf("Yes, it shall be\n");
-	case '3':
-		printf("Fortune favors the bold\n");
-	case '4':
-		printf("Good, good - do it\n");
-	case '5':
-		printf("No, not this time\n");
-	case '6':
-		printf("Not now, but when you least expect it, yes\n");
-	case '7':
-		printf("Never in a million years\n");
-	case '8':
-		printf("Even I am not powerful enough to know\n");
-	default:
-		printf("You have chosen poorly\n");
+	while ((c = getchar()) != '\n') {
+		switch (c) {
+		case '1':
+			printf("Sometimes good, sometimes shit\n");
+			break;
+		case '2':
+			printf("Yes, it shall be\n");
+			break;
+		case '3':
+			printf("Fortune favors the bold\n");
+			break;
+		case '4':
+			printf("Good, good - do it\n");
+			break;
+		case '5':
+			printf("No, not this time\n");
+			break;
+		case '6':
+			printf("Not now, but when you least expect it, yes\n");
+			break;
+		case '7':
+			printf("Never in a million years\n");
+			break;
+		case '8':
+			printf("Even I am not powerful enough to know\n");
+			break;
+		default:
+			printf("You have chosen poorly\n");
+			break;
+		}
 	}
 	return 0;
 }
