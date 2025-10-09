fe4c2b0f614c9c9125f61248d07d18d3a2980159
matt box <mdbox037a@protonmail.com>
2025-10-08 23:31:12 -0400

    create segmentation fault generator

--- KnR/chapter_3/show_escapes.c
+++ KnR/chapter_3/show_escapes.c
@@ -13,23 +13,39 @@ int main() {
 	// function to accept input and store in an array
 	// then display the results
 	char result[4], string[MAX_STR_LEN];
-	int c, status;
+	int i, j, k, c, status;
 
-	printf("Enter a string up to 99 characters long: \n");
-	for (int i = 0; (c = getchar()) != EOF && i < MAX_STR_LEN - 2; ++i) {
+	printf("Enter a string up to 99 characters long: ");
+	for (int i = 0; (c = getchar()) != EOF && i < MAX_STR_LEN - 2;) {
 		status = escape(c, result);
 		if (status) {
 			string[i++] = result[0];
-			string[i] = result[1];
+			string[i++] = result[1];
 		} else
-			string[i] = c;
+			string[i++] = c;
 	}
-
+	// string[++i] = '\0';
 	printf("\nYour string with escape characters displayed: %s\n", string);
+
+	// clear string contents
+	for (j = 0; j < MAX_STR_LEN - 1; ++j)
+		string[j] = '\0';
+
+	printf("Now enter a string with some written newlines and tabs: ");
+	for (int k = 0; (c = getchar()) != EOF && k < MAX_STR_LEN - 1; ++k) {
+		status = unescape(c, result);
+		if (status)
+			string[k] = result[0];
+		else
+			string[k] = c;
+	}
+	string[k] = '\0';
+	printf("\nYour string with no escapes: %s\n", string);
+
 	return 0;
 }
 
-char escape(char c, char result[]) {
+char escape(char c, char result[4]) {
 	switch (c) {
 	case '\n':
 		result[0] = '\\';
@@ -46,8 +62,15 @@ char escape(char c, char result[]) {
 	}
 }
 
-char unescape(char c, char result[]) {
+char unescape(char c, char result[4]) {
 	// function to take a character from main() and if an escape character,
 	// return \ such that the end text will display the \n or \t
-	return 0;
+	switch (c) {
+	case '\\':
+		result[0] = '\\';
+		result[1] = '\0';
+		return 1;
+	default:
+		return 0;
+	}
 }
