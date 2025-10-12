823ae69bc837ac1d480f48a39f36b5fe6cce5c7a
matt box <mdbox037a@protonmail.com>
2025-10-12 19:01:20 -0400

    add compressed version of for loop with single line for all three ops in the reverse

--- KnR/chapter_3/commas.c
+++ KnR/chapter_3/commas.c
@@ -10,3 +10,10 @@ void reverse(char s[]) {
 		s[j] = c;
 	}
 }
+
+void reverse_compressed(char s[]) {
+	int c, i, j;
+
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+		c = s[i], s[i] = s[j], s[j] = c;
+}
