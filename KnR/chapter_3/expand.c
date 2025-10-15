3121a4f6ec72749c1e6b0fd84e7af79cf3d9a469
matt box <mdbox037a@protonmail.com>
2025-10-14 21:52:34 -0400

    complete first successful logic area to handle expanding a-z into full alphabet

--- KnR/chapter_3/expand.c
+++ KnR/chapter_3/expand.c
@@ -5,20 +5,40 @@
  * leading or trailing - is taken literally */
 
 #include <stdio.h>
+#include <string.h>
 #define MAX_S1_LEN 100
 #define MAX_S2_LEN 1000
-#define ALPHABET "abcdefghijklmnopqrstuvwxyz"
-#define DIGITS "0123456789"
+
+void expand(char s1[], char s2[]);
+
+int main() {
+	char s1[MAX_S1_LEN] = "test a-z test";
+	char s2[MAX_S2_LEN];
+
+	expand(s1, s2);
+	printf("s1: %s\n", s1);
+	printf("s2: %s\n", s2);
+}
 
 void expand(char s1[], char s2[]) {
-	int c, i, j;
+	int c, i, j, k;
 	char buffer[4] = {0};
+	const char *ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+	const char *DIGITS = "0123456789";
 
-	for (i = 1, j = 1; s1[i] != '\0' && j < MAX_S2_LEN - 2; ++i, ++j) {
+	for (i = 1, j = 0; s1[i] != '\0' && j < MAX_S2_LEN - strlen(ALPHABET);
+	     ++i) {
 		if (s1[i - 1] == 'a' && s1[i] == '-' && s1[i + 1] == 'z') {
-			// TODO: figure out how to copy in multiple chars
-			// starting from array position
-			s2[j - 1] = ALPHABET;
+			for (k = 0; k < strlen(ALPHABET); ++k) {
+				s2[j] = ALPHABET[k];
+				++j;
+			}
+			i += 2; // jump ahead to get past the '-z'
+		} else {
+			s2[j] = s1[i - 1];
+			++j;
 		}
 	}
+	s2[j++] = s1[i - 1];
+	s2[j] = '\0';
 }
