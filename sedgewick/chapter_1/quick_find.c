9bfc56a3f042b53998ffa5e9b2e070bf31695110
matt box <mdbox037a@protonmail.com>
2025-10-21 22:53:28 -0400

    resolve \n issue with scanf statements and added connections checking module

--- sedgewick/chapter_1/quick_find.c
+++ sedgewick/chapter_1/quick_find.c
@@ -2,16 +2,29 @@
 #define N 10
 
 int main() {
-	int i, p, q, t, id[N];
+	int i, p, q, t, id[N], x, y;
 
 	for (i = 0; i < N; i++) // initialize array
 		id[i] = i;
-	while (scanf("%d %d\n", &p, &q) == 2) {
+	printf("Input connections list:\n");
+	while (scanf("%d %d", &p, &q) != EOF) {
 		if (id[p] == id[q])
 			continue;
 		for (t = id[p], i = 0; i < N; i++)
 			if (id[i] == t)
 				id[i] = id[q];
-		printf(" %d %d\n", p, q);
+		// printf(" %d %d\n", p, q);
 	}
+	clearerr(stdin);
+	printf("Check connections:\n");
+	while (scanf("%d %d", &x, &y) == 2) {
+		if (id[x] == id[y])
+			printf("Connection successful!\n");
+		else
+			printf("Connection unsuccessful...\n");
+	}
+
+	printf("Array contents:\n");
+	for (i = 0; i < N; i++)
+		printf("%d: %d\n", i, id[i]);
 }
