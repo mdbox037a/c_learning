c3b5f1c096544312a40bba5ac0a75f6cf8b72ee7
matt box <mdbox037a@protonmail.com>
2025-10-27 22:42:52 -0400

    begin work to implement rightmost occurence minigrep program

---
+++ KnR/chapter_4/minigrep_right.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int mini_getline(char line[], int max);
+int strindex(char source[], char searchfor[], int str_len);
+
+char pattern[] = "ould";
+
+/* find all lines matching a pattern */
+int main() {
+	char line[MAXLINE];
+	int found = 0;
+	int str_len = 0;
+
+	str_len = mini_getline(line, MAXLINE);
+	while (str_len > 0)
+		if (strindex(line, pattern, str_len) >= 0) {
+			printf("%s", line);
+			found++;
+		}
+
+	return found;
+}
+
+/* getline: get line into s[], return length */
+int mini_getline(char s[], int lim) {
+	int c, i;
+
+	i = 0;
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if (c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+
+	return i;
+}
+
+/* strindex: return index of t in s, -1 if none */
+int strindex(char s[], char t[], int str_len) {
+	int i, j, k;
+	// TODO: implement right-start, using string length
+	for (i = 0; s[i] != '\0'; i++) {
+		for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+			;
+		if (k > 0 && t[k] == '\0')
+			return i;
+	}
+	return -1;
+}
