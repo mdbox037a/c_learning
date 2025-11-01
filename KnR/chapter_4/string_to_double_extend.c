2b8aa3f341f66994ec45b0c350bb152e958cda99
matt box <mdbox037a@protonmail.com>
2025-10-31 23:33:37 -0400

    begin extension of atof to handle scientific notation

---
+++ KnR/chapter_4/string_to_double_extend.c
@@ -0,0 +1,33 @@
+#include <ctype.h>
+
+/* atof: convert string s to double */
+double atof(char s[]) {
+	double val, power, e, esign;
+	int i, sign;
+
+	for (i = 0; isspace(s[i]); i++) // skip whitespace
+		;
+	sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	for (val = 0.0; isdigit(s[i]); i++)
+		val = 10.0 * val + (s[i] - '0');
+	if (s[i] == '.')
+		i++;
+	for (power = 1.0; isdigit(s[i]); i++) {
+		val = 10.0 * val + (s[i] - '0');
+		power *= 10;
+	}
+	// TODO: implement scientific notation handler
+	// PLANNING: construct multiplier with correct number of zeroes
+	// PLANNING: then tack on to original return
+	if (s[i] == 'e' || s[i] == 'E') {
+		i++;
+		esign = (s[i] == '-') ? -1 : 1;
+		for (e = 1.0; isdigit(s[i]); i++) {
+			// TODO: implement logic
+			;
+		}
+	}
+	return sign * val / power;
+}
