d7f408d180de4287184eda360ae1537b261591cf
matt box <mdbox037a@protonmail.com>
2025-11-05 20:46:04 -0500

    implement getch and ungetch; complete simple calculator

--- KnR/chapter_4/push_pop_calculator.c
+++ KnR/chapter_4/push_pop_calculator.c
@@ -101,3 +101,19 @@ int getop(char s[]) {
 		ungetch(c);
 	return NUMBER;
 }
+
+// NOTE: getch() and ungetch()
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; // buffer for ungetch
+int bufp = 0;      // next free position in buf
+
+int getch(void) { return (bufp > 0) ? buf[--bufp] : getchar(); }
+
+void ungetch(int c) {
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		bufp[bufp++] = c;
+}
