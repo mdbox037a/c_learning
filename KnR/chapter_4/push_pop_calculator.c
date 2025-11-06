96c7e021dbb1fc18ecc8e3841eb8728da6901766
matt box <mdbox037a@protonmail.com>
2025-11-05 20:29:57 -0500

    implement getop()

--- KnR/chapter_4/push_pop_calculator.c
+++ KnR/chapter_4/push_pop_calculator.c
@@ -51,6 +51,7 @@ int main() {
 
 #define MAXVAL 100
 
+/* sp and val-stack external b/c they need to be shared by push and pop */
 int sp = 0;         // next free stack position
 double val[MAXVAL]; // value stack
 
@@ -72,4 +73,31 @@ double pop(void) {
 	}
 }
 
-// TODO: add getop()
+// NOTE: getop()
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next character or numeric operand */
+int getop(char s[]) {
+	int i, c;
+
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+	if (!isdigit(c) && c != '.')
+		return c;
+	i = 0;
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if (c == '.')
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
