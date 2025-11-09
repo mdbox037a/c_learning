e79b1f938d7ab0da434b833a34899c5e7f0d3d89
matt box <mdbox037a@protonmail.com>
2025-11-08 22:27:26 -0500

    implement command to duplicate top element of the stack

--- KnR/chapter_4/ppc_4-4.c
+++ KnR/chapter_4/ppc_4-4.c
@@ -19,6 +19,7 @@ int main() {
 	double op2;
 	char s[MAXOP];
 
+	printf("commands: ? print top; & dup top; $ clear stack\n");
 	while ((type = getop(s)) != EOF) {
 		switch (type) {
 		case NUMBER:
@@ -51,6 +52,11 @@ int main() {
 		case '?':
 			op2 = pop();
 			printf("\t%.8g\n", op2);
+			push(op2);
+			break;
+		case '&':
+			op2 = pop();
+			push(op2);
 			push(op2);
 			break;
 		case '\n':
