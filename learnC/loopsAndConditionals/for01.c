f56de17776ab450276a63c94504162be2056393f
matt box <mdbox037a@protonmail.com>
2025-11-16 20:19:22 -0500

    convert back to while loop for practice

--- learnC/loopsAndConditionals/for01.c
+++ learnC/loopsAndConditionals/for01.c
@@ -4,9 +4,9 @@ int main() {
 
 	int i = 99;
 
-	for (i = 99; i >= 0; i--) {
+	while (i > 0) {
 		printf("%d bottles of pop on the wall.\n", i);
 		printf("Take one down and pass it around\n");
-		printf("%d bottles of pop on the wall.\n", i - 1);
+		printf("%d bottles of pop on the wall.\n", --i);
 	}
 }
