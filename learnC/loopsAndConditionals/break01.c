3758245e3f3dea6588e08c6ebc6c56dc4770e239
matt box <mdbox037a@protonmail.com>
2025-11-16 20:45:30 -0500

    create quick break example

---
+++ learnC/loopsAndConditionals/break01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+	int number1 = 1;
+	int numbers_entered1 = 0;
+
+	while (numbers_entered1 < 10) {
+
+		printf("Loop 1 - Please enter a number: ");
+		scanf("%d", &number1);
+
+		// Figure out how to break out here!
+		if (number1 <= 0)
+			break;
+
+		numbers_entered1++;
+	}
+	printf("Good job! You’ve broken out!\n");
+}
