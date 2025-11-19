2a365fae1a9af2768d3bad775ccae7db9639bacf
matt box <mdbox037a@protonmail.com>
2025-11-18 20:51:13 -0500

    complete first, very brief exercise to create and print a pointer

--- learnC/pointersAndMemory/pointers01.c
+++ learnC/pointersAndMemory/pointers01.c
@@ -2,8 +2,11 @@
 
 int main() {
 	// Checkpoint 1 code goes here.
+	double dblVar;
 
 	// Checkpoint 2 code goes here.
+	double *dblPtr = &dblVar;
 
 	// Checkpoint 3 code goes here.
+	printf("%p", dblPtr);
 }
