a65433da4ee6b171f2b4302e56980dd740de7864
matt box <mdbox037a@protonmail.com>
2025-11-19 21:38:38 -0500

    create second pointers exercise skeleton

---
+++ learnC/pointersAndMemory/pointers02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+	double g = 9.81;
+	double pi = 3.14;
+
+	// Checkpoint 1 code goes here.
+	double *dblPtr = &g;
+	// Checkpoint 2 code goes here.
+	printf("%p", dblPtr);
+	// Checkpoint 3 code goes here.
+	dblPtr = &pi;
+}
