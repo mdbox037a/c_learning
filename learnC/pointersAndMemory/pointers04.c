5a6bc4e6692b8feed282b24bb601310b73e56ea3
matt box <mdbox037a@protonmail.com>
2025-11-21 23:15:56 -0500

    complete all pointer arithmetic assignments

--- learnC/pointersAndMemory/pointers04.c
+++ learnC/pointersAndMemory/pointers04.c
@@ -2,17 +2,24 @@
 #include <string.h>
 
 int main() {
+	int i;
 	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	char s[] =
 	    "Farmer Jack realized that big yellow quilts were expensive!!";
 
 	// Code for Checkpoint 1 goes here.
-
+	int *ptr = &arr[9];
 	// Code for Checkpoint 2 goes here.
-
+	for (i = 10; i > 0; i--) {
+		printf("%i\n", *ptr);
+		ptr--;
+	}
 	// Code for Checkpoint 3 goes here.
-
+	char *ptr2 = &s[0];
 	// Code for Checkpoint 4 goes here.
-
+	for (i = 0; i < strlen(s); i++) {
+		*ptr2 = '#';
+		ptr2++;
+	}
 	printf("%s\n", s);
 }
