5c5dec198c7f5e38b662d97441f4818ebf81c2ad
matt box <mdbox037a@protonmail.com>
2025-11-23 16:40:05 -0500

    implement more flexible getRandomNumber() to allow main() to call with maxNumber arg

--- learnC/functionsAndStructures/funcSigs02.c
+++ learnC/functionsAndStructures/funcSigs02.c
@@ -3,14 +3,14 @@
 #include <time.h>
 
 // Modify the code below
-int getRandomNumber() {
-	int randomNumber = rand() % 1000 + 1;
+int getRandomNumber(int maxNumber) {
+	int randomNumber = rand() % maxNumber + 1;
 	return randomNumber;
 }
 
 int main(void) {
 	srand(time(NULL));
 	// Modify the code below
-	int randomNumber = 0;
+	int randomNumber = getRandomNumber(100);
 	printf("My random number is: %d", randomNumber);
 }
