d8b8ffdcb7a375bc27f711b89f5daf49665f8dd3
matt box <mdbox037a@protonmail.com>
2025-11-23 16:30:46 -0500

    complete function definition and call example

--- learnC/functionsAndStructures/funcSigs.c
+++ learnC/functionsAndStructures/funcSigs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 // Write your code below
+void printMyFavoriteAnimal(void) { printf("My favorite animal is a dog\n"); }
 
-int main(void) { ; }
+int main(void) { printMyFavoriteAnimal(); }
