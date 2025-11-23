8e93fa93bc139dda738ccd609edc5c32cea72449
matt box <mdbox037a@protonmail.com>
2025-11-23 16:46:46 -0500

    complete function example that manipulates variable with dereference via pointer

--- learnC/functionsAndStructures/funcWithPointers.c
+++ learnC/functionsAndStructures/funcWithPointers.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 // Write your code below
+void incrementAge(int *agePointer) { *agePointer += 1; }
 
-int main(void) {}
+int main(void) {
+	int age = 37;
+	incrementAge(&age);
+	printf("%d\n", age);
+}
