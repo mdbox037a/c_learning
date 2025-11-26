adef4d24f51c139f6168b7e4f919baa2c5b9e633
matt box <mdbox037a@protonmail.com>
2025-11-25 20:38:38 -0500

    complete table struct definition and initialize two test instances

--- learnC/functionsAndStructures/struct03.c
+++ learnC/functionsAndStructures/struct03.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 // Write your code below
+struct Table {
+	int length;
+	int width;
+	int height;
+	char color[20];
+};
 
 int main(void) {
 	int table1Length = 24;
@@ -14,4 +20,6 @@ int main(void) {
 	char table2Color[20] = "Matte Black";
 
 	// Write your code below
+	struct Table table1 = {24, 24, 20, "Dark Brown"};
+	struct Table table2 = {42, 18, 32, "Matte Black"};
 }
