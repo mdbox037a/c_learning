cff8986cf18330e8216f9478ed8e09353b2080e5
matt box <mdbox037a@protonmail.com>
2025-11-25 21:14:39 -0500

    use pointer dereference with dot notation and then arrow notation for updating struct elements

--- learnC/functionsAndStructures/struct05.c
+++ learnC/functionsAndStructures/struct05.c
@@ -9,6 +9,11 @@ int main(void) {
 	struct Person person1 = {"Martin", 57};
 	struct Person person2 = {"Michelle", 27};
 	// Write your code below
+	struct Person *person1Pointer = &person1;
+	struct Person *person2Pointer = &person2;
+
+	(*person1Pointer).age += 1;
+	person2Pointer->age += 10;
 
 	printf("Hello my name is %s and I am %d years old.\n", person1.name,
 	       person1.age);
