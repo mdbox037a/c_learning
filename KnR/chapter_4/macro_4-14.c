54bc0c45026f3dcd9bb6efbde0c9b7236ade5ed9
matt box <mdbox037a@protonmail.com>
2025-12-23 20:42:45 -0500

    complete first macro with convention of do {...} while(0) for safety

--- KnR/chapter_4/macro_4-14.c
+++ KnR/chapter_4/macro_4-14.c
@@ -1,4 +1,10 @@
 /* Exercise 4-14: Define a macro swap(t, x, y) that interchanges two arguments
  * of type t. (Block structure will help) */
 
-#define swap(t, x, y) ;
+#define swap(t, x, y)                                                          \
+	do {                                                                   \
+		t _swapper_var;                                                \
+		_swapper_var = x;                                              \
+		x = y;                                                         \
+		y = _swapper_var;                                              \
+	} while (0)
