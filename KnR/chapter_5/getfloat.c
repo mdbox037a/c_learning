8f53c9218f1f33e0452e827656778f58c2a19587
matt box <mdbox037a@protonmail.com>
2025-12-25 21:06:47 -0500

    complete logic to handle floating point numbers with getfloat() - add decimal portion to whole portion proegressiovely using pow()

--- KnR/chapter_5/getfloat.c
+++ KnR/chapter_5/getfloat.c
@@ -1,15 +1,16 @@
-/* Exercise 5-2: Write getfloat(), the floating-oint analog of getint().
+/* Exercise 5-2: Write getfloat(), the floating-point analog of getint().
  * What type does getfloat() return as its function value */
 
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 
 int getch(void);
 void ungetch(int);
 
 /* getint: get next integer from input into *pn */
-int getint(int *pn) {
-	int c, sign;
+int getint(float *pn) {
+	int c, sign, places;
 
 	while (isspace(c = getch()))
 		;
@@ -20,8 +21,11 @@ int getint(int *pn) {
 	sign = (c == '-') ? -1 : 1;
 	if (c == '+' || c == '-')
 		c = getch();
-	for (*pn = 0; isdigit(c); c = getch())
+	for (*pn = 0; isdigit(c) && c != '.'; c = getch())
 		*pn = 10 * *pn + (c - '0');
+	/* now handle fractional portion of number following . */
+	for (places = 1; isdigit(c); c = getch(), places++)
+		*pn = *pn + ((c - '0') * (pow(0.1, places)));
 	*pn *= sign;
 	if (c != EOF)
 		ungetch(c);
