9c94db450cafee6176188b814049bb40279012f6
matt box <mdbox037a@protonmail.com>
2025-12-25 20:38:14 -0500

    create copy of getint() to start work on getfloat() from exercise 5-2

---
+++ KnR/chapter_5/getfloat.c
@@ -0,0 +1,29 @@
+/* Exercise 5-2: Write getfloat(), the floating-oint analog of getint().
+ * What type does getfloat() return as its function value */
+
+#include <ctype.h>
+#include <stdio.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getint: get next integer from input into *pn */
+int getint(int *pn) {
+	int c, sign;
+
+	while (isspace(c = getch()))
+		;
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c);
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-')
+		c = getch();
+	for (*pn = 0; isdigit(c); c = getch())
+		*pn = 10 * *pn + (c - '0');
+	*pn *= sign;
+	if (c != EOF)
+		ungetch(c);
+	return c;
+}
