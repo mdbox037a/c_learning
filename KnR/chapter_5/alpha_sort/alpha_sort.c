01577c78344be1f176c4601a3616433da248b07f
matt box <mdbox037a@protonmail.com>
2026-01-04 19:23:11 -0500

    continue study of knr chapter 5 line alpha sort function

--- KnR/chapter_5/alpha_sort/alpha_sort.c
+++ KnR/chapter_5/alpha_sort/alpha_sort.c
@@ -7,3 +7,49 @@ char *lineptr[MAXLINES]; /* array of pointers to each line start */
 
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+/* sort input lines */
+void main(void) {
+	int nlines; /* number of input lines to read */
+
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+		qsort(lineptr, 0, nlines - 1);
+		writelines(lineptr, nlines);
+		return 0;
+	} else {
+		printf("error: input too big to sort\n");
+		return 1;
+	}
+}
+
+#define MAXLEN 1000
+int section1_9_getline(char *, int);
+char *alloc(int);
+
+/* readlines: read input lines */
+int readlines(char *lineptr[], int maxlines) {
+	int len, nlines;
+	char *p, line[MAXLEN];
+
+	nlines = 0;
+	while ((len = getline(line, MAXLEN)) > 0) {
+		if (nlines >= maxlines || p = alloc(len) == NULL)
+			return -1;
+		else {
+			line[len - 1] = '\0'; /* delete newline */
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
+		return nlines;
+	}
+}
+
+/* writelines: write output lines */
+void writelines(char *lineptr[], int nlines) {
+	int i;
+
+	for (i = 0; i < nlines; i++)
+		printf("%s\n", lineptr[i]);
+}
