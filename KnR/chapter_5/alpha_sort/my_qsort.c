4989f64e39314dc4f790d886a0d6b9b882437def
matt box <mdbox037a@protonmail.com>
2026-01-06 17:00:46 -0500

    implement small quicksort program in project

---
+++ KnR/chapter_5/alpha_sort/my_qsort.c
@@ -0,0 +1,29 @@
+#include <string.h>
+
+static void swap(char *v[], int i, int j);
+
+/* my_qsort: sort v[left]...v[right] into increasing order */
+void my_qsort(char *v[], int left, int right) {
+	int i, last;
+
+	if (left >= right)
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+
+	for (i = left + 1; i <= right; i++)
+		if (strcmp(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	swap(v, left, last);
+	my_qsort(v, left, last - 1);
+	my_qsort(v, last + 1, right);
+}
+
+/* swap: interchange v[i] and v[j] */
+static void swap(char *v[], int i, int j) {
+	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
