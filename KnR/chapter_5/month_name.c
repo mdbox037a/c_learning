a7fcb8de8bee8f504e19f8610544157822dd9a8a
matt box <mdbox037a@protonmail.com>
2026-01-10 21:39:29 -0500

    create alternate version of month_day as exercise to dig deeper into read-only and read-write data

--- KnR/chapter_5/month_name.c
+++ KnR/chapter_5/month_name.c
@@ -7,3 +7,14 @@ char *month_name(int n) {
 
 	return (n < 1 || n > 12) ? name[0] : name[n];
 }
+
+/* attempting to do the same as above - take up more space, but able to edit
+ * months now? */
+char *different_month_name(int n) {
+	static char name[13][15] = {
+	    "Illegal month", "January",  "February", "March",  "April",
+	    "May",           "June",     "July",     "August", "September",
+	    "October",       "November", "December"};
+
+	return (n < 1 || n > 12) ? name[0] : name[n];
+}
