7281d9a1f7633ab4ccc1b4553b29139093896bb9
matt box <mdbox037a@protonmail.com>
2026-01-12 20:25:24 -0500

    complete quick daily c exercise - plane ticket total

---
+++ boot.dev/ticket_total.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int baseFare = 120;
+	int luggageFee = 30;
+	int hasCoupon = 1;
+
+	int totalBeforeDiscount;
+	int finalTotal;
+
+	totalBeforeDiscount = baseFare + luggageFee;
+	if (hasCoupon)
+		finalTotal = totalBeforeDiscount - 20;
+	else
+		finalTotal = totalBeforeDiscount;
+
+	printf("%d\n", totalBeforeDiscount);
+	printf("%d\n", finalTotal);
+
+	return 0;
+}
