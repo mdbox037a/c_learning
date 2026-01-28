5c9e51f749688de7f9530972fa4f93ae3292fe49
matt box <mdbox037a@protonmail.com>
2026-01-28 08:09:36 -0500

    draft pseudo code for first try at solving problem

--- leetcode/13_roman_to_integer.c
+++ leetcode/13_roman_to_integer.c
@@ -1,3 +1,13 @@
 /* 13 - Roman to Integer: given a roman numeral, convert it to an integer */
 
-int romanToInt(char *s) { return 0; }
+int romanToInt(char *s) {
+	/* planning:
+	 * 1) create parallel array where each index holds integer equivalent
+	 * of each char in s
+	 * 2) find end of integer array and move towards beginning
+	 * 3) add each integer to total, except if current index is less than
+	 * previous index -> subtract in that case
+	 * 4) return total */
+
+	return 0;
+}
