f4f1bbd3c4ef2fe205de57f2638ad8dca94425e1
Matt Box <128308984+mdbox037a@users.noreply.github.com>
2026-07-20 23:43:23 -0400

    Update sizeof.c

--- learnC/arraysAndStrings/sizeof.c
+++ learnC/arraysAndStrings/sizeof.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  double arr[] = {1.0, 8, 2.27, 16.2, 2, 3.14, 12.6, 1.62, 6.54, 9, 11};
+  double arr[] = {1.0, 8, 2.29, 16.2, 2, 3.14, 12.6, 1.62, 6.54, 9, 11};
   int len;
 
   // Checkpoint 1 code goes here.
